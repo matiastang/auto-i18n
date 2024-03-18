cc3f7f8b710bd61028fff5709a61788010a3d183
唐道勇 <matias@tangdaoyongdeMacBook-Pro.local>
2024-03-18 17:47:31 +0800

    feat: - 接入gulp

---
+++ gulpfile.ts/index.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: matiastang
+ * @Date: 2021-12-24 14:50:33
+ * @LastEditors: matiastang
+ * @LastEditTime: 2024-03-18 17:47:04
+ * @FilePath: /auto-i18n/gulpfile.ts/index.ts
+ * @Description: gulpfile.js
+ */
+// 导出所有 tasks
+export * from './tasks/npm'
+export * from './tasks/scripts'
+export * from './tasks/version'
+export * from './tasks/git'
+// export * from './tasks/vueTheme'
